58a3ff4f23255e9f5ea0403b42e3698ec8877c18
wayne254-crypto <137960649+wayne254-crypto@users.noreply.github.com>
2026-08-17 05:48:39 -0700

    Add files via upload
    Beginner series sql statement tutorial, select statement sql

---
+++ 2.select statement tutorial.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM Parks_and_recreation.employee_demographics;
+
+SELECT first_name,last_name, birth_date,age,(age + 10)* 4
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT first_name, gender
+FROM Parks_and_recreation.employee_demographics
+
+
+
+
+
+
