8535c71690ee3a0a2e126eaf20c82e57c0a45483
wayne254-crypto <137960649+wayne254-crypto@users.noreply.github.com>
2026-08-18 03:28:01 -0700

    Add files via upload

---
+++ group by,my sql tutorial.sql
@@ -0,0 +1,20 @@
+-- GROUP BY
+SELECT *
+FROM employee_demographics;
+
+
+SELECT gender, AVG(age),MAX(age),MIN(age),COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+-- ORDER BY
+SELECT*
+FROM employee_demographics
+ORDER BY  AGE,GENDER;
+
+
+
+
+
+
